aa140a501f167a03bdcd3a217dbe19baaf14813b
nader <nadnonn12@gmail.com>
2025-03-25 03:26:23 -0400

    removed unneeded code

--- equine-welfareTests/equine_welfareTests.swift
+++ equine-welfareTests/equine_welfareTests.swift
@@ -3,22 +3,6 @@ import XCTest
 @testable import Horse_App
 
 struct Horse_AppTests {
-    // unit testing to check that the functions work correct, still incorrect
-    func testAddingHorse() {
-        let initialCount = horseData.horses.count
-        horseData.addHorse(name: "Test Horse", breed: "Test Breed", time: 4)
-
-        XCTAssertEqual(horseData.horses.count, initialCount + 1, "Horse should be added to the list")
-        XCTAssertEqual(horseData.horses.last?.name, "Test Horse", "Horse name should match")
-        }
-
-    func testDeletingHorse() {
-        horseData.addHorse(name: "Test Horse", breed: "Test Breed", time: 4)
-        let initialCount = horseData.horses.count
-
-        horseData.deleteHorse(at: IndexSet(integer: initialCount - 1))
-
-        XCTAssertEqual(horseData.horses.count, initialCount - 1, "Horse should be deleted")
-        }
-
+    //unit testing
+    func testExample() {
 }
